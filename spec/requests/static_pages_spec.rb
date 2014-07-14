ee2951b2f9d5f252b4cc486e7618973178b93142
heynicecode <rickymuldrew@gmail.com>
2014-07-13 19:07:28 -0500

    Create Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	let(:base_title) { "Tweeter" }
+	
 	describe "Home page" do
 		it "should have the content 'Tweeter'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Tweeter')
 		end
 
-		it "should have the right title" do
+		it "should have the correct title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Tweeter | Home")
+			expect(page).to have_title("#{base_title} | Home")
+		end
+	end
+
+	describe "About page" do
+		it "should have the content 'About Tweeter'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About')
+		end
+
+		it "should have the correct title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("#{base_title} | About")
 		end
 	end
 
@@ -19,21 +33,21 @@ describe "Static pages" do
 			expect(page).to have_content('Help')
 		end
 
-		it "should have the right title" do
+		it "should have the correct title" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Tweeter | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
-	describe "About page" do
-		it "should have the content 'About Tweeter'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Tweeter')
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
 		end
 
-		it "should have the right title" do
-			visit '/static_pages/about'
-			expect(page).to have_title("Tweeter | About")
+		it "should have the correct title" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
 end
